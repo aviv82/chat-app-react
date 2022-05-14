90a397ef865aa5580889854e772932989517df30
Aviv <aviv.dror@gmail.com>
2022-05-14 23:26:57 +0200

    can fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Fetch } from "./api/Fetch";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const HandleFetch = async () => {
+    const toFetch = await Fetch("tests");
+    console.log("test", toFetch.data);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={HandleFetch}>test</button>
+    </div>
+  );
 }
 
 export default App;
